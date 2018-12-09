db5c94a1fff238de1c80b5af732dacb790c79091
NakagamiYuta <guguru0014@gmail.com>
2018-12-09 19:16:11 +0900

    [fix]ファイルのなかに不要な文字列があったので削除

--- Assets/Scripts/MoveSkyBox.cs
+++ Assets/Scripts/MoveSkyBox.cs
@@ -2,34 +2,8 @@
 using System.Collections;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-#if UNITY_EDITOR
 using UnityEditor;
-#endif
 
-<<<<<<< HEAD
-namespace UnityEngine.UI
-{
-    public class GraphicCast : Graphic
-    {
-
-        protected override void OnFillVBO(System.Collections.Generic.List<UIVertex> vbo)
-        {
-            base.OnFillVBO(vbo);
-            vbo.Clear();
-        }
-#if UNITY_EDITOR
-
-        [CustomEditor(typeof(GraphicCast))]
-        class GraphicCastEditor : Editor
-        {
-            public override void OnInspectorGUI()
-            {
-            }
-        }
-
-#endif
-    }
-=======
 public class MoveSkyBox : MonoBehaviour {
 	float _curRot = 0f;
 	float rotSpeed = 5f;
@@ -44,6 +18,5 @@ public class MoveSkyBox : MonoBehaviour {
 		_curRot %= 360f;
 		RenderSettings.skybox.SetFloat("_Rotation", _curRot);
 	}
->>>>>>> 264332f48f97d4004dba65c50a03aeecb62df2c4
 }
 
