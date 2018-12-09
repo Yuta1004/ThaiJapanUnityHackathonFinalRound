c49c1ba0ceaa2212bf3d62d16b83f6b18282a4a6
NakagamiYuta <guguru0014@gmail.com>
2018-12-09 19:35:09 +0900

    [add]タイトルシーンから各シーンへ遷移するように

---
+++ Assets/Scripts/ViewHelpButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewHelpButton : MonoBehaviour {
+
+	public void OnClick() {
+//		SceneManager.LoadScene("stage1");
+	
+		FadeManager mFadeManager = FadeManager.Instance;
+		mFadeManager.LoadScene("help", 2.0f);
+	}
+}
