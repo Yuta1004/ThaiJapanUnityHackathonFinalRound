d9f19955a4cdade8c3e414a8bd033c86ed3c8f3c
kishida kazuki <k.k02kamatyo@gmail.com>
2018-12-10 20:54:30 +0900

    [add]特定の場所でスペースを押すと電気が貯めれるようにした
    親子関係まじ許さん

--- Assets/TestScripts/GetPlayerEvent.cs
+++ Assets/TestScripts/GetPlayerEvent.cs
@@ -3,24 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GetPlayerEvent : MonoBehaviour {
-
-    private string ObjectTag;
+    
 	private void OnCollisionStay(Collision collision){
 
         //Event発行
-        ObjectTag = collision.gameObject.tag;
-        if(ObjectTag == "Soil"){
+        if(collision.gameObject.tag == "Soil"){
             if (Input.GetKeyDown(KeyCode.Space)){
                 this.gameObject.GetComponent<PlayerEvent>().GetSpace(collision.gameObject);
-            }
-        }else if(ObjectTag == "House"){
-            if (Input.GetKeyDown(KeyCode.Space)){
-                this.gameObject.GetComponent<HouseAttribute>().GetSpace(this.gameObject);
             }
         }
 	}
 
 	private void OnTriggerStay(Collider other){
+        
+        if (Input.GetKeyDown(KeyCode.Space)){
+            if (other.gameObject.tag == "House"){
+                //Debug.Log(other.gameObject.transform.root.gameObject.transform.GetChild(1).gameObject.name);
+                other.gameObject.transform.root.gameObject.transform.GetChild(1).GetComponent<HouseAttribute>().GetSpace(this.gameObject);
+            }
+        }
 		
 	}
 }
--- Assets/TestScripts/HouseAttribute.cs
+++ Assets/TestScripts/HouseAttribute.cs
@@ -6,6 +6,7 @@ public class HouseAttribute : MonoBehaviour {
 
     public int electricQuantity;
     public int getElectric;
+    //private int Time = 0;
 
     public void GetSpace(GameObject Player){
         int getTree = Player.GetComponent<PlayerEvent>().passTree();
