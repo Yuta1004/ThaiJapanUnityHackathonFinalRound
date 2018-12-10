16acd47dabf6fa3d28fc32b1ecd5997b0bbe70f2
NakagamiYuta <guguru0014@gmail.com>
2018-12-10 11:38:30 +0900

    [add]Unity-chan!を動かすように

---
+++ Assets/Scripts/DoAnimationCharacter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoAnimationCharacter : MonoBehaviour {
+	public String animationName;
+	private Animator animator;
+	
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent (typeof(Animator)) as Animator;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!animator.GetCurrentAnimatorStateInfo(0).shortNameHash.Equals(Animator.StringToHash(animationName))){
+			animator.Play(animationName);
+		}
+	}
+	
+	public void OnCallChangeFace(){}
+
+}
