b7957312d4d2500af0078878f093bdbca044ee8d
NakagamiYuta <guguru0014@gmail.com>
2018-12-10 20:26:10 +0900

    [add]Titleでボタンが押されるとUnity-chan!が走り出すようになった

---
+++ Assets/Scripts/DashToSpaceShip.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashToSpaceShip : MonoBehaviour {
+	private bool doDash = false;
+	
+	// Update is called once per frame
+	void Update () {
+		if(doDash) gameObject.transform.Translate(0, 0, 0.1f);
+	}
+
+	void SendMessage(string message) {
+		doDash = true;
+	}
+	
+	public void OnCallChangeFace(){}
+}
