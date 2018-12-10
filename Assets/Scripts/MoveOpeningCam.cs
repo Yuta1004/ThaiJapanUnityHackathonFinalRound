9173b4d08fa77fc6f81b7745cd5c89b2f8a730a1
NakagamiYuta <guguru0014@gmail.com>
2018-12-10 10:56:41 +0900

    [add]カメラを動かすように

---
+++ Assets/Scripts/MoveOpeningCam.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOpeningCam : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Translate(-31f, -21f, 94);
+	}
+}
