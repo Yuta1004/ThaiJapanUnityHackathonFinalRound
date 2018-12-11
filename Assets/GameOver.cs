5f94076b737f3f0f5f1d425237d9fc0e58e5ed85
kishida kazuki <k.k02kamatyo@gmail.com>
2018-12-11 10:47:35 +0900

    [new]ゲームオーバーを追加

---
+++ Assets/GameOver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+
+    private float CountTime;
+	// Use this for initialization
+	void Start () {
+        CountTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CountTime += Time.deltaTime;
+        Debug.Log(CountTime);
+        
+        if(CountTime > 60){
+            Debug.Log("GAMEOVER");
+        }
+	}
+}
