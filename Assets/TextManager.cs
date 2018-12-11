e4648e479e34b517dc0a375fd6e5b9081f9b8e39
kishida kazuki <k.k02kamatyo@gmail.com>
2018-12-11 11:27:23 +0900

    [add]

---
+++ Assets/TextManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextManager : MonoBehaviour {
+
+    public Text Text1;
+    public Text Text2;
+    public Text Text3;
+    public Text Text4;
+    public Text Text5;
+    public Text Text6;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
